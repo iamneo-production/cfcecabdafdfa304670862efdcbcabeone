866cf075fbc61bc7b4c336abaad3df5a92c9fc09
Iamneo <techadmin@iamneo.ai>
2023-09-03 07:54:47 +0000

    Backup Commit

---
+++ tic.js
@@ -0,0 +1,23 @@
+function myfunc() {
+    var b1, b2, b3, b4, b5,b6, b7, b8, b9;
+    bl= document.getElementById("b1").value; 
+    b2= document.getElementById("b2").value;
+    b3= document.getElementById("b3").value;
+    b4= document.getElementById("b4").value; 
+    b5 =document.getElementById("b5").value; 
+    b6= document.getElementById("b6").value;
+    b7= document.getElementById("b7").value;
+    b8= document.getElementById("b8").value;
+    b9= document.getElementById("b9").value;
+
+    var blbtn, b2btn, b3btn, b4btn, b5btn, b6btn, b7btn, b8btn, b9btn;
+   blbtn = document.getElementById("b1");
+   b2btn = document.getElementById("b2");
+   b3btn = document.getElementById("b3");
+   b4btn = document.getElementById("b4");
+   b5btn = document.getElementById("b5"); 
+   b6btn = document.getElementById("b6");
+   b7btn = document.getElementById("b7");
+
+b8btn = document.getElementById("b8"); b9btn = document.getElementById("b9");
+}
